b874d69f546844cda90b1a8eff84fb411cc575d2
Steven Jay <stevenjasionowicz@gmail.com>
2017-01-13 15:50:58 +1100

    Add Todos from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
+import $ from 'jquery';
 import AddProject from './components/AddProject';
 import Projects from './components/Projects';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
@@ -10,32 +13,78 @@ class App extends Component {
       projects: []
     }
   }
-  componentWillMount() {
+
+  getTodos() {
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/todos',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({todos: data}, function(){
+          console.log(this.state);
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+      }
+    });
+  }
+
+  getProjects() {
     this.setState({ projects: [
       {
+          id:uuid.v4(),
           title: 'Front-end dev',
           category: 'Web Dev'
         },
         {
+          id:uuid.v4(),
           title: 'UX',
           category: 'Design'
         },
         {
+          id:uuid.v4(),
           title: 'Server Adim',
           category: 'Dev ops'
         },
         {
+          id:uuid.v4(),
           title: 'Copywriter',
           category: 'marketing'
         }
     ]});
+    
+  }
+
+  componentWillMount() {
+    this.getProjects();
+    this.getTodos();
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  handleAddProject(project) {
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id) {
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
   }
 
   render() {
     return (
       <div className="App">
-        <AddProject />
-        <Projects projects={this.state.projects} />
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
+        <hr />
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
--- src/components/Project_item.js
+++ src/components/Project_item.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
 class ProjectItem extends Component {
+  deleteProject(id){
+  	this.props.onDelete(id);
+  }
   render() {
-  	console.log(this.props);
+  	// console.log(this.props);
     return (
       <li className="Projectlist">
-      	{this.props.project.title} - {this.props.project.category}
+      	{this.props.project.title} - {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>X</a>
       </li>
     );
   }
+}
+
+ProjectItem.propTypes = {
+	project: React.PropTypes.object,
+	onDelete: React.PropTypes.func
 }
 
 export default ProjectItem;
